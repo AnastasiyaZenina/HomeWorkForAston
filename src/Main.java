802b89568445749e74b9fc181f1e9a0540de752f
Anastasiya Zenina <stasya.soloveva.2014@mail.ru>
2024-05-31 16:56:00 +0300

    исправление 3 задания

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Employee employee1 = new Employee("Дэниэл Киз", "писатель", "daniel.kiz@mail.ru", "79531002346", 64000, 34);
         employee1.employeeInfo();
-    }
 
+        Park.Attractions attractions1 = new Park().new Attractions("Паровозик", "10-17:30", 140.50);
+        attractions1.attractionsInfo();
+    }
 }
--- src/Park.java
+++ src/Park.java
@@ -1,13 +1,22 @@
 
 public class Park {
-    private String nameOfTheAttraction;
-    private int workingHours;
-    private int price;
 
-    public Park(String nameOfTheAttraction, int workingHours, int price) {
-        this.nameOfTheAttraction = nameOfTheAttraction;
-        this.workingHours = workingHours;
-        this.price = price;
+    public class Attractions {
+        private String nameOfTheAttraction;
+        private String workingHours;
+        private double price;
 
+        public  Attractions(String nameOfTheAttraction, String workingHours, double price) {
+            this.nameOfTheAttraction = nameOfTheAttraction;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+
+        public void attractionsInfo() {
+            System.out.println("Аттракцион: " + nameOfTheAttraction);
+            System.out.println("Часы работы: " + workingHours);
+            System.out.println("Стоимость: " + price);
+
+        }
     }
 }
